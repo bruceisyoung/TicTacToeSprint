4145a15be0109db6d9cd32dd9cb09e6eb865b34a
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-12-05 15:09:36 -0800

    add simple sample to ensure server is running

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path =  require('path');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + './src/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 })
 
 app.listen(3000, function () {
