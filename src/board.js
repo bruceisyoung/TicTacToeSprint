497b7858b34668b5979cb1121f554c186b8b6e52
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-12-05 15:20:14 -0800

    add some basic board logic

---
+++ src/board.js
@@ -0,0 +1,14 @@
+// build an board function, which is used to generate an empty board;
+
+var board = function(n) {
+  n = n || 3;
+  var gridArr = [];
+
+  for (var i = 0; i < n; i++) {
+  	for (var j = 0; j < n; j++) {
+      gridArr[i][j] = 0;
+  	}
+  }
+  
+  return gridArr;
+}
